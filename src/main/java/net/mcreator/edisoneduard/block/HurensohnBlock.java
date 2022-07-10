f8dc91321d73ea2f7170bd449038860ab53160df
dgRen333 <reneh@host.docker.internal>
2022-07-10 17:35:50 +0200

    added block of hurensohn

---
+++ src/main/java/net/mcreator/edisoneduard/block/HurensohnBlock.java
@@ -0,0 +1,58 @@
+
+package net.mcreator.edisoneduard.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.world.IBlockReader;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.edisoneduard.EdisonEduardModElements;
+
+import java.util.List;
+import java.util.Collections;
+
+@EdisonEduardModElements.ModElement.Tag
+public class HurensohnBlock extends EdisonEduardModElements.ModElement {
+	@ObjectHolder("edison_eduard:hurensohn")
+	public static final Block block = null;
+
+	public HurensohnBlock(EdisonEduardModElements instance) {
+		super(instance, 1);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items
+				.add(() -> new BlockItem(block, new Item.Properties().group(ItemGroup.BUILDING_BLOCKS)).setRegistryName(block.getRegistryName()));
+	}
+
+	public static class CustomBlock extends Block {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.IRON).sound(SoundType.METAL).hardnessAndResistance(1.5f, 10f).setLightLevel(s -> 0));
+			setRegistryName("hurensohn");
+		}
+
+		@Override
+		public int getOpacity(BlockState state, IBlockReader worldIn, BlockPos pos) {
+			return 15;
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(this, 1));
+		}
+	}
+}
